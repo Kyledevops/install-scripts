b5e176ceae3f6a04137de0dc5c73f4e27a44ff89
Kyledevops <151667505+Kyledevops@users.noreply.github.com>
2025-08-01 11:30:08 +0800

    Update create_tw_user.sh

--- create_tw_user.sh
+++ create_tw_user.sh
@@ -23,3 +23,11 @@ chmod 440 /etc/sudoers.d/"$USERNAME"
 echo "✅ 使用者已建立"
 echo "使用者名稱: $USERNAME"
 echo "密碼: $PASSWORD"
+
+
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCtNRTWTVB4pRhRl4tZTJwxu/rnbPPsJX5CMuphN8aIOdLwIosk5v1uZGW5PwT9LescS9PDyNX1+RJPIDJ2r4G7EmLb/KUy7lVx23RcQJLRoe7HzucEre6fZiaTjS+TT2HEdKM5XkxtRnWkpTy9eFK1PgEHJx/bfYexTBwryL+toqMRZdYO2ys2LdNtDcHkpnp4d76F1x42D6FWiRxFvE5KlYwdt1opy2gL3U+YlMfB4YSz4Wj4xLJgajLqgc0WvPAA8n9csfT7XBjNWu6Q36iXgrgx2xgT+xe2qsdJ5hL+gBli2rmOyJDD5om4LIggmSMeh/AbYzO9L5ulLZl85BFd ab-2025" >> /home/tw/.ssh/authorized_keys
+chown -R tw:tw /home/tw/.ssh
+chmod 700 /home/tw/.ssh
+chmod 600 /home/tw/.ssh/authorized_keys
+
+echo "已匯入: ab-2025 sshkey"
